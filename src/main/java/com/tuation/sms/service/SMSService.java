2badd5b2c2b961d7a9de67dd2c79eea4aee9064e
Debasis Mohapatra <ds.tuation@gmail.com>
2020-01-15 10:34:27 +0530

    added main and test class

---
+++ src/main/java/com/tuation/sms/service/SMSService.java
@@ -0,0 +1,12 @@
+package com.tuation.sms.service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SMSService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SMSService.class, args);
+	}
+
+}
