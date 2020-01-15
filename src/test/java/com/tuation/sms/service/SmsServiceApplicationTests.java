747e10c34930f78aebecf229cd366d5770a54b5d
Debasis Mohapatra <ds.tuation@gmail.com>
2020-01-15 09:47:40 +0530

    Initial Commit

---
+++ src/test/java/com/tuation/sms/service/SmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuation.sms.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
